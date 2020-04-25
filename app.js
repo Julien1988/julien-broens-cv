728938abbe2d24b296d91c7c0792081a069bc784
Julien <julienbroens@gmail.com>
2020-04-25 12:13:21 +0200

    création de la liste des skills avec un setTimeout

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
   let getDomElementSubtitle = getDomElement.children[1];
   let skillsArray = ["M", "y", " ", "S", "k", "i", "l", "l", "s"];
   let count;
+  let index;
 
   let skillsArrayIcones = [
     "<i class='fab fa-php'></i>",
@@ -23,7 +24,6 @@
     getDomElementTitle.innerHTML = " ";
     getDomElementSubtitle.innerHTML = " ";
     changeTitle(skillsArray);
-    showSkills();
   });
 
   // Génération d'un nombre aléatoire pour l'écriture du texte
@@ -35,55 +35,27 @@
   function changeTitle() {
     getDomElementTitle.innerHTML += skillsArray[count];
     count++;
+    skillsButton.remove();
     if (count < skillsArray.length) {
       setTimeout(changeTitle, getRandomArbitrary(100, 300));
+    } else {
+      showSkills();
     }
   }
 
   // Affiche la liste des skills
+  index = 0;
+  let ul = getDomElement.appendChild(document.createElement("ul"));
   function showSkills() {
-    skillsButton.remove();
-    let ul = getDomElement.appendChild(document.createElement("ul"));
+    let li = ul.appendChild(document.createElement("li"));
     ul.className = "main__section__container__list";
+    li.className = "main__section__container__list__item";
+    li.innerHTML += skillsArrayIcones[index];
 
-    for (let i = 0; i < skillsArrayIcones.length; i++) {
-      let li = ul.appendChild(document.createElement("li"));
-      li.className = "main__section__container__list__item";
-      li.innerHTML = skillsArrayIcones[i];
+    if (index < skillsArrayIcones.length - 1) {
+      index++;
+
+      setTimeout(showSkills, 1000);
     }
-
-    // let li = ul.appendChild(document.createElement("li"));
-    // li.className = "main__section__container__list__item";
   }
 })();
-
-{
-  /* <section class="main__section section section-top">
-<h1 class="main__section__title">My Skills</h1>
-<div class="main__section__container">
-  <ul class="main__section__container__list">
-    <li class="main__section__container__list__item">
-      <i class="fab fa-php"></i>
-    </li>
-    <li class="main__section__container__list__item">
-      <i class="fab fa-css3-alt"></i>
-    </li>
-    <li class="main__section__container__list__item">
-      <i class="fab fa-html5"></i>
-    </li>
-    <li class="main__section__container__list__item">
-      <i class="fab fa-js"></i>
-    </li>
-    <li class="main__section__container__list__item">
-      <i class="fab fa-sass"></i>
-    </li>
-    <li class="main__section__container__list__item">
-      <i class="fab fa-wordpress"></i>
-    </li>
-    <li class="main__section__container__list__item">
-      and more...
-    </li>
-  </ul>
-</div>
-</section> */
-}
